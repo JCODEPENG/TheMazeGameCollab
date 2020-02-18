d230dfa91bbb689f9d0d4641c6fc1ab8979fa9b3
Joshua Peng <46659122+JCODEPENG@users.noreply.github.com>
2020-02-17 22:18:58 -0800

    Stumped
    Stuck on the maze generator, it's just printing one straight line up and down and not completing.

--- MazeGame/src/com/company/MazeGenerator.java
+++ MazeGame/src/com/company/MazeGenerator.java
@@ -4,8 +4,8 @@ import java.util.Random;
 import java.util.Stack;
 
 public class MazeGenerator {
-    private int Height = 18;
-    private int Width = 13;
+    private int Height = 13;
+    private int Width = 18;
     private int Maze[][] = new int[15][20];
     private int WallSymbol = 3;
     private int PathSymbol = 4;
@@ -26,7 +26,31 @@ public class MazeGenerator {
         while (!VisitedYCoords.isEmpty() && !VisitedXCoords.isEmpty()){
             int CurrentXCoord = VisitedXCoords.pop();
             int CurrentYCoord = VisitedYCoords.pop();
-            
+            if (CurrentXCoord + 2 <= Width && this.Maze[CurrentYCoord][CurrentXCoord+2] == WallSymbol){
+                this.Maze[CurrentYCoord][CurrentXCoord+1] = PathSymbol;
+                this.Maze[CurrentYCoord][CurrentXCoord+2] = PathSymbol;
+                VisitedXCoords.push(CurrentXCoord + 2);
+                VisitedYCoords.push(CurrentYCoord);
+            }
+            else if (CurrentXCoord - 2 > 0 && this.Maze[CurrentYCoord][CurrentXCoord-2] == WallSymbol){
+                this.Maze[CurrentYCoord][CurrentXCoord-1] = PathSymbol;
+                this.Maze[CurrentYCoord][CurrentXCoord-2] = PathSymbol;
+                VisitedXCoords.push(CurrentXCoord - 2);
+                VisitedYCoords.push(CurrentYCoord);
+            }
+            else if (CurrentYCoord + 2 <= Height && this.Maze[CurrentYCoord+2][CurrentXCoord] == WallSymbol){
+                this.Maze[CurrentYCoord+1][CurrentXCoord] = PathSymbol;
+                this.Maze[CurrentYCoord+2][CurrentXCoord] = PathSymbol;
+                VisitedXCoords.push(CurrentXCoord);
+                VisitedYCoords.push(CurrentYCoord + 2);
+            }
+            else if (CurrentYCoord - 2 > 0 && this.Maze[CurrentYCoord-2][CurrentXCoord] == WallSymbol){
+                this.Maze[CurrentYCoord-1][CurrentXCoord] = PathSymbol;
+                this.Maze[CurrentYCoord-2][CurrentXCoord] = PathSymbol;
+                VisitedXCoords.push(CurrentXCoord);
+                VisitedYCoords.push(CurrentYCoord - 2);
+            }
+
 
         }
 
