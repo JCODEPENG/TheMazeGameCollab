f6e6a8695d25be5aab65ddc60bc2985d7eacf194
Joshua Peng <46659122+JCODEPENG@users.noreply.github.com>
2020-02-17 21:32:27 -0800

    Printed 20x18 grid of just 3's
    Still in the progress of implementing the maze generating algorithm. But decided to push what i have for now.

--- MazeGame/src/com/company/GameHandler.java
+++ MazeGame/src/com/company/GameHandler.java
@@ -3,9 +3,8 @@ package com.company;
 public class GameHandler {
 
     public static void main(String[] args) {
-        Cat Object = new Cat(1, 2, 3);
-        Object.MoveRandom();
-        int printout = Object.GetX();
-        System.out.println(printout);
+        MazeGenerator Maze = new MazeGenerator();
+
+        Maze.OutputMaze();
     }
 }
--- MazeGame/src/com/company/MazeGenerator.java
+++ MazeGame/src/com/company/MazeGenerator.java
@@ -1,5 +1,63 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class MazeGenerator {
-    
+    private int Height = 18;
+    private int Width = 13;
+    private int Maze[][] = new int[15][20];
+    private int WallSymbol = 3;
+    private int PathSymbol = 4;
+    private Stack<Integer> VisitedXCoords = new Stack();
+    private Stack<Integer> VisitedYCoords = new Stack();
+
+    //private GetNext()
+
+    public MazeGenerator(){
+
+        this.MazeFill();
+        Random choice = new Random();
+        int StartingPointX = choice.nextInt(Width);
+        int StartingPointY = choice.nextInt(Height);
+        Maze[StartingPointY][StartingPointX] = PathSymbol;
+        VisitedXCoords.push(StartingPointX);
+        VisitedYCoords.push(StartingPointY);
+        while (!VisitedYCoords.isEmpty() && !VisitedXCoords.isEmpty()){
+            int CurrentXCoord = VisitedXCoords.pop();
+            int CurrentYCoord = VisitedYCoords.pop();
+            
+
+        }
+
+
+
+    }
+
+
+
+    public void MazeFill(){
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 20; j++) {
+                this.Maze[i][j] = WallSymbol;
+                this.Maze[i][j] = WallSymbol;
+
+            }
+        }
+    }
+    public void OutputMaze(){
+        for (int i = 0; i < 15; i++){
+            for (int j = 0; j < 20; j++){
+                System.out.print(this.Maze[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public int[][] ReturnMaze(){
+        return this.Maze;
+    }
+
+
+
 }
