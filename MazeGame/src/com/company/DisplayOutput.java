2d4b866816219b0f8f4b7c47ff941042fa7cb84b
Joshua Peng <46659122+JCODEPENG@users.noreply.github.com>
2020-02-25 00:18:49 -0800

    OutputMsgsCompleted
    Added the Output Msgs, this should be all of them.

--- MazeGame/src/com/company/DisplayOutput.java
+++ MazeGame/src/com/company/DisplayOutput.java
@@ -26,6 +26,9 @@ public class DisplayOutput {
                 if (Maze[i][j] == 6){
                     System.out.println('C');
                 }
+                if (Maze[i][j] == 7){
+                    System.out.println('X');
+                }
             }
             System.out.println();
         }
@@ -35,7 +38,34 @@ public class DisplayOutput {
 
     public void WelcomeMsg(){
         System.out.println("----------------------------------------");
-        System.out.println("Welcome to Cat and Mouse Maze Adventure!\n By Joshua Peng and Jocelyn Gau");
+        System.out.println("Welcome to Cat and Mouse Maze Adventure!\nBy Joshua Peng and Jocelyn Gau");
         System.out.println("----------------------------------------");
+    }
+
+    public void HelpMsg(){
+        System.out.println("Directions:\n\tFind 5 cheese before a cat eats you!");
+        System.out.println("Legend:\n\t#: Wall\n\t@: You (a mouse)\n\t!: Cat\n\t$: Cheese\n\t.: Unexplored space");
+        System.out.println("Moves:\n\tUse W (up), A (left), S (down) and D (right) to move.\n\t(You must press enter after each move).");
+    }
+    public void CheeseCollected(int NumberCollected, int TotalNeeded){
+        System.out.println("Cheese collected: " + NumberCollected + " of "+ TotalNeeded);
+    }
+
+    public void GetInputMsg(){
+        System.out.print("Enter your move [WASD?]: ");
+    }
+    public void WinMsg(){
+        System.out.println("Congratulations! You won!");
+    }
+    public void InvalidMoveMsg(){
+        System.out.println("Invalid move: you cannot move through walls!");
+    }
+
+    public void GotEatenMsg(){
+        System.out.println("I'm sorry, you have been eaten!");
+    }
+
+    public void GameOverMsg(){
+        System.out.println("GAME OVER; please try again.");
     }
 }
