a86ce6c4217d1fa8b8f7b9bd227ae61f1c006e23
Joshua Peng <46659122+JCODEPENG@users.noreply.github.com>
2020-02-24 23:09:21 -0800

    HitWallMsg
    Added some output messages.

---
+++ MazeGame/src/com/company/DisplayOutput.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class DisplayOutput {
+    public void OutputMaze(int[][] Maze){
+        assert(Maze != null);
+        int[] YDimension = Maze[0];
+        int YSize = YDimension.length;
+        int XSize = Maze.length;
+        for  (int i = 0; i < YSize; i++){
+            for (int j = 0; j < XSize; j++){
+                if (Maze[i][j] == 1){
+                    System.out.println('.');
+                }
+                if (Maze[i][j] == 2){
+                    System.out.println(' ');
+                }
+                if (Maze[i][j] == 3){
+                    System.out.println('#');
+                }
+                if(Maze[i][j] == 4){
+                    System.out.println('@');
+                }
+                if (Maze[i][j] == 5){
+                    System.out.println('!');
+                }
+                if (Maze[i][j] == 6){
+                    System.out.println('C');
+                }
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    public void hitWallMsg(){
+        System.out.println("----------------------------------------");
+        System.out.println("Welcome to Cat and Mouse Maze Adventure!\n By Joshua Peng and Jocelyn Gau");
+        System.out.println("----------------------------------------");
+    }
+}
